c5f8a432c2f7cb1241a09c876a88966e673465b6
Tsitola NOMENJANAHARY <tsito.fifa@gmail.com>
2023-03-18 13:20:08 +0100

    Routage des pages dans la classe main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'package:examen_mobile/pages/home.dart';
 import 'package:examen_mobile/pages/loading.dart';
+import 'package:examen_mobile/pages/about.dart';
+import 'package:examen_mobile/pages/CreerDevis.dart';
+import 'package:examen_mobile/pages/DevisAuto.dart';
+import 'package:examen_mobile/pages/decouverte.dart';
 
 void main() => runApp(MaterialApp(
   title: 'Voyage',
@@ -9,6 +13,10 @@ void main() => runApp(MaterialApp(
   routes: {
     '/': (context) => const LoadingApp(),
     '/home' : (context) => const Home(),
+    '/découverte' : (context) => const Decouverte(),
+    '/créer_devis' : (context) => const CreerDevis(),
+    '/devis_auto' : (context) => const DevisAuto(),
+    '/about' : (context) => const About(),
   },
 ));
 
